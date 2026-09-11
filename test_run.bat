3f289ce82a6349e020faafabe5e69fb217938532
BloatwareGuard <developer@example.com>
2026-09-11 20:42:17 +0900

    cleanup: remove stale publish dir, test bat files, debug logs

--- test_run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "c:\Users\HP\bloatware-guard\src\bin\Release\net8.0-windows\win-x64"
-timeout /t 3 >nul
-BloatwareGuard.exe --version < nul
-echo ExitCode: %errorlevel%
--- test_version.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "c:\Users\HP\bloatware-guard\src\bin\Release\net8.0-windows\win-x64"
-BloatwareGuard.exe --version
